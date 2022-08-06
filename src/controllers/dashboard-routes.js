c014436866c5c0889e200f5b8972be3dbf7f7a40
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-08-05 20:12:33 -0700

    updated with new functionality

--- src/controllers/dashboard-routes.js
+++ src/controllers/dashboard-routes.js
@@ -3,7 +3,23 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment, Blog } = require('../models');
 
 router.get('/', (req, res) => {
+    Post.findAll({
+        where: {
+            //use id from session
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM blog WHERE post.id = blog.post_id'), 'blog_count']
+        ],
+    })
     res.render('dashboard', { loggedIn: true });
+    
 });
+
+
 
 module.exports = router;
