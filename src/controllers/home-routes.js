1406eb0908df5a0acff1dfe3686291908dc56c8a
schad huntsman <schadhuntsman@gmail.com>
2022-09-29 01:18:21 -0700

    Update home-routes.js

--- src/controllers/home-routes.js
+++ src/controllers/home-routes.js
@@ -1,81 +1,126 @@
 const router = require('express').Router();
-const { rest } = require('lodash');
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Blog } = require('../models');
-const { count } = require('../models/Post');
-const withAuth = require('../utils/auth');
+const { User, Post, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
-    Post.findAll({
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM blog WHERE post.id = blog.post_id)'), 'blog_count']
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'content',
+      'created_at'
+    ],
+    include: 
+    [{
+      model: Comment,
+      attributes: ['username']
+    },
+  ],
+    
+    include: [
+      {
+        model: User,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
           model: User,
           attributes: ['username']
         }
-      ]
+      },
+    {
+      model: User,
+      attributes: ['username']
+    }
+  ]
+})
+.then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({
+        plain: true
+      }));
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn
+      }):
     })
-      .then(dbPostData => {
-        // pass a single post object into the homepage template
-        res.render('homepage', {
-          posts,
-          loggedIn: req.session.loggedIn
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
+    
+
+//get single
+//create user
+router.post('/', withAuth, (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+})
+
+
+router.post('/login', withAuth, (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that username' 
         });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+          return;
+        }
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+      });
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+    
+        if (!validPassword) {
+          res.status(400).json({ message: 'Incorrect credentials!'
+        });
+          return;
+     }
+  });
+
+  req.session.save(() => {
+    req.session.user_id = dbUserData.id;
+    req.session.username = dbUserData.username;
+    req.session.loggedIn = true;
+
+    res.json({ user: dbUserData,
+               message: 'You are now logged in!' 
       });
   });
 
-router.get('/post/:id', (req, res) => {
-  const post = {
-    id: 1,
-    post_url: 'https://handlebarsjs.com/guide/',
-    title: 'Handlebars Docs',
-    created_at: new Date(),
-    blog_posts: count,
-    comments: [{}, {}],
-    user: {
-      username: 'test_user'
-    }
-  };
 
-  res.render('single-post', { post });
+router.post('/logout', withAuth, (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
-
-
-
-  router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/')
-        return;
-    }
-
-    res.render('login');
-  });
-  
-if  (req.session.loggedIn) {
-  req.session.destroy(() => {
-    res.status(204).end();
-  });
-}
-else {
-  res.status(404).end();
-}
 
 module.exports = router;
